6f0d271c3c60dd9871cfe8c26c57dc5b0f376a3d
LASBENNES Lucas <lucas.lasbennes@ynov.com>
2025-10-01 12:05:26 +0200

    feat(server.js): Added a simple express server with a "Hello world" route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`📡 Server started on port http://localhost:${PORT}`);
+});
