475edeef11b9ab6b109ffa7561068134b03cb116
LASBENNES Lucas <lucas.lasbennes@ynov.com>
2025-10-09 12:16:39 +0200

    fix(eslint): update indentation rule from 2 to 4 spaces

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,23 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default defineConfig([
-  {
-    files: ['**/*.js'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser
-      }
-    }
-  },
-  {
-    rules: {
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2],
-      'no-unused-vars': 'warn'
+    {
+        files: ['**/*.js'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser
+            }
+        }
     },
-  },
+    {
+        rules: {
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'indent': ['error', 4],
+            'no-unused-vars': 'warn'
+        },
+    },
 ]);
